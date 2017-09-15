47de2bcbc04aa20502c66e91f1b5975c791cb56e
amritakhamkar <amrita.khamkar@springernature.com>
2017-09-15 15:44:13 +0530

    change in floder structure

---
+++ Assignment_amrita/js/script.js
@@ -0,0 +1,18 @@
+$('.progressBar').each(function () {
+  var progress = $(this);
+  var percentage = Math.ceil($(this).attr('data-percentage'));
+  $({countNum: 0}).animate({countNum: percentage}, {
+    duration: 1000,
+    easing:'swing',
+    step: function() {
+      // What todo on every count
+    var pct = '';
+    if(percentage == 0){
+      pct = Math.floor(this.countNum) + '%';
+    }else{
+      pct = Math.floor(this.countNum+1) + '%';
+    }
+    progress.css('width',pct);
+    }
+  });
+});
